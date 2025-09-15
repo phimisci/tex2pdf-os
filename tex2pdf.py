24e19741fb627cf98427b892b05fa6811cfd4089
Felix Kopecky <kopeckyf@users.noreply.github.com>
2025-09-15 11:36:01 +0200

    Support Latexmk workflow for new layout

--- tex2pdf.py
+++ tex2pdf.py
@@ -2,11 +2,15 @@ import argparse, subprocess, os, shutil
 
 parser = argparse.ArgumentParser(description='Basic py script to execute TEX2PDF transformation.')
 parser.add_argument('file', type=str, help='The filename of the TeX-file.')
+parser.add_argument("--layout", type=str, help="The layout in use")
 
 args = parser.parse_args()
 
 # File path
 file_path = f"/app/{args.file}"
+
+# Layout version
+layout_version = args.layout
 
 # Check if file exists
 if not os.path.isfile(file_path):
@@ -28,16 +32,31 @@ def run_xelatex():
     result = subprocess.run(texlive_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     return result
 
+def run_latexmk():
+    latexmk_command = ["latexmk", "-lualatex", "-diagnostics", 
+                       "-output-directory=/app/output", file_path]
+    result = subprocess.run(latexmk_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    return result
+
 # Run xelatex twice for correct citation rendering
-result = run_xelatex()
-if result.returncode == 0:
-    print("First XeLaTeX run started successfully")
+if args.layout == "classic":
     result = run_xelatex()
     if result.returncode == 0:
-        print("Second XeLaTeX run started successfully")
+        print("First XeLaTeX run started successfully")
+        result = run_xelatex()
+        if result.returncode == 0:
+            print("Second XeLaTeX run started successfully")
+        else:
+            print("Error during second XeLaTeX run")
+            print(result.stderr)
     else:
-        print("Error during second XeLaTeX run")
+        print("Error during first XeLaTeX run")
         print(result.stderr)
-else:
-    print("Error during first XeLaTeX run")
-    print(result.stderr)
+
+if args.layout == "twocolumn":
+    result = run_latexmk()
+    if result.returncode == 0:
+        print("Latexmk ran successfully")
+    else:
+        print("Error during Latexmk run")
+        print(result.stderr)
